b4aef8fb49868e873b82bc6b4db75b8950e8d836
jiku <yokogawa32@gmail.com>
2020-03-14 18:11:00 +0900

    現状でコミット

--- app/src/main/java/com/example/calendar_no1/MainActivity.kt
+++ app/src/main/java/com/example/calendar_no1/MainActivity.kt
@@ -6,41 +6,33 @@ import android.os.Bundle
 import android.view.Menu
 import android.view.MenuInflater
 import android.widget.CalendarView
+import android.widget.ImageView
 import android.widget.LinearLayout
 import androidx.annotation.RequiresApi
 import kotlinx.android.synthetic.main.activity_main.*
 
+
 class MainActivity : AppCompatActivity() {
 
-    @RequiresApi(Build.VERSION_CODES.HONEYCOMB)
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
-
-        // CalendarViewの生成
         val calendarView = CalendarView(this)
         calendarView.layoutParams = LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT)
 
-        // 親レイアウトに、CalendarViewを追加
-        val linearLayout = findViewById<LinearLayout>(R.id.container)
-        linearLayout.addView(calendarView)
 
-//        // Button切り替え用のフラグ
-//        var flag = false
 
-        // リスナーをボタンに登録
-//        button.setOnClickListener {
-//
-//            if (flag) {
-//                // flagがtrueの時
-//
-//                flag = false
-//            } else {
-//                // flagがfalseの時
-//                textview.setText(getString(R.string.world))
-//                flag = true
+
+//        val imageView2 = findViewById<ImageView>(R.id.addButton)
+//        imageView2.setImageResource(R.drawable.ic_launcher_background)
+
+
+
+
+
 //            }
 //        }
     }
