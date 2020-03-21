0f103d342c2d18b1f341f49e86ff8c4afac5d776
jiku <yokogawa32@gmail.com>
2020-03-22 00:51:44 +0900

    レイアウト追加 試合情報CSVも上げときます。

--- app/src/main/java/com/example/calendar_no1/MainActivity.kt
+++ app/src/main/java/com/example/calendar_no1/MainActivity.kt
@@ -5,14 +5,17 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuInflater
-import android.widget.CalendarView
-import android.widget.ImageView
-import android.widget.LinearLayout
+import android.widget.*
 import androidx.annotation.RequiresApi
 import kotlinx.android.synthetic.main.activity_main.*
+import java.io.BufferedReader
+import java.io.InputStreamReader
 
 
 class MainActivity : AppCompatActivity() {
+
+    //試合情報配列
+    var game: Array<String> = emptyArray<String>()
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,25 +23,49 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        //カレンダービューの生成
         val calendarView = CalendarView(this)
-        calendarView.layoutParams = LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT)
+        calendarView.layoutParams = LinearLayout.LayoutParams(
+            LinearLayout.LayoutParams.MATCH_PARENT,
+            LinearLayout.LayoutParams.WRAP_CONTENT
+        )
+
+        //csvの読み取り
+        readCsv("baseballGames.csv")
+
+        //現在日付が取れるはず。。。だけどとれてないかな？
+        calendarView.setOnDateChangeListener { view, year, month, dayOfMonth ->
+            val date = "$year/$month/$dayOfMonth"
+            val textView: TextView = findViewById(R.id.textView1)
+            textView.text = date
 
 
+            //Toast.makeText(this, date, Toast.LENGTH_SHORT).show()
+        }
+        val textView: TextView = findViewById(R.id.textView2)
+        textView.text = game[2]
 
-
-//        val imageView2 = findViewById<ImageView>(R.id.addButton)
-//        imageView2.setImageResource(R.drawable.ic_launcher_background)
-
-
-
-
-
-//            }
-//        }
+        calendarView.findViewById<CalendarView>(R.id.calendar)
+        calendarView.setOnDateChangeListener { view, year, month, dayOfMonth ->
+            val date = "$year/$month/$dayOfMonth"
+            val textView: TextView = findViewById(R.id.textView1)
+            textView.text = date
+            //Toast.makeText(this, date, Toast.LENGTH_SHORT).show()
+        }
     }
-//    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-//        val inflater: MenuInflater = menuInflater
-//        inflater.inflate(R.menu.menu, menu)
-//        return true
-//    }
+
+    fun readCsv(filename: String) {
+
+        val file = resources.assets.open(filename)
+        val fileReader = BufferedReader(InputStreamReader(file))
+        var i: Int = 0
+        fileReader.forEachLine {
+            if (it.isNotBlank()) {
+                game = it.split(",").toTypedArray()
+            }
+            i++;
+        }
+
+
+    }
 }
