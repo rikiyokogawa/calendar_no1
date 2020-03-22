bcbee4dec14c3b0cf392d0cca6081c9e3edbe06c
jiku <yokogawa32@gmail.com>
2020-03-22 21:27:16 +0900

    ボタンクリックしたらダイアログでるはず

--- app/src/main/java/com/example/calendar_no1/MainActivity.kt
+++ app/src/main/java/com/example/calendar_no1/MainActivity.kt
@@ -1,13 +1,10 @@
 package com.example.calendar_no1
 
-import android.os.Build
-import androidx.appcompat.app.AppCompatActivity
+import android.app.AlertDialog
+import android.content.DialogInterface
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuInflater
 import android.widget.*
-import androidx.annotation.RequiresApi
-import kotlinx.android.synthetic.main.activity_main.*
+import androidx.appcompat.app.AppCompatActivity
 import java.io.BufferedReader
 import java.io.InputStreamReader
 
@@ -38,20 +35,38 @@ class MainActivity : AppCompatActivity() {
             val date = "$year/$month/$dayOfMonth"
             val textView: TextView = findViewById(R.id.textView1)
             textView.text = date
-
-
             //Toast.makeText(this, date, Toast.LENGTH_SHORT).show()
-        }
-        val textView: TextView = findViewById(R.id.textView2)
-        textView.text = game[2]
 
-        calendarView.findViewById<CalendarView>(R.id.calendar)
-        calendarView.setOnDateChangeListener { view, year, month, dayOfMonth ->
-            val date = "$year/$month/$dayOfMonth"
-            val textView: TextView = findViewById(R.id.textView1)
-            textView.text = date
-            //Toast.makeText(this, date, Toast.LENGTH_SHORT).show()
+            // ボタンを設定
+            val button = findViewById<Button>(R.id.fab) as Button
+            button.setOnClickListener {
+                val editText = EditText(this)
+                editText.hint = "編集できるよ！"
+                AlertDialog.Builder(this)
+                    .setTitle("タイトル")
+                    .setMessage("メッセージ")
+                    .setView(editText)
+                    .setPositiveButton("OK",
+                        DialogInterface.OnClickListener { dialog, which ->
+                            // お好きな処理をどうぞ
+                        })
+                    .show()
+            }
+
+
+            val textView1: TextView = findViewById(R.id.textView2)
+            textView.text = game[2]
+
+            calendarView.findViewById<CalendarView>(R.id.calendar)
+            calendarView.setOnDateChangeListener { view, year, month, dayOfMonth ->
+                val date = "$year/$month/$dayOfMonth"
+                val textView: TextView = findViewById(R.id.textView1)
+                textView.text = date
+                //Toast.makeText(this, date, Toast.LENGTH_SHORT).show()
+            }
         }
+
+
     }
 
     fun readCsv(filename: String) {
